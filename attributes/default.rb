468b6d9a9d93401b4bbb79a1ad4fb648d655f74e
Greg Karékinian <greg@karekinian.com>
2019-07-05 15:42:13 +0200

    Fix the linting warnings

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ node.default['ipfs']['version'] = '0.4.21'
 node.default['ipfs']['checksum'] = 'a7ec5ddc4d52f818cbf3853a80f7ec17f9fde9128f039485dbe1889cf673d562'
 
 node.default['ipfs']['ulimit'] = 64000
-node.default['ipfs']['memory_max'] = "512M"
+node.default['ipfs']['memory_max'] = '512M'
 node.default['ipfs']['config'] = {
   # Do not contact local network addresses. This will stop platforms like Hetzner
   # to block your server (https://github.com/ipfs/go-ipfs/issues/1226)
@@ -22,10 +22,10 @@ node.default['ipfs']['config'] = {
                           '/ip4/203.0.113.0/ipcidr/24',
                           '/ip4/240.0.0.0/ipcidr/4'],
   # Set up the Gateway to be writable
-  "Gateway.Writable" => true,
+  'Gateway.Writable' => true,
 
   # Set up CORS headers
-  "API.HTTPHeaders.Access-Control-Allow-Credentials" => ["true"],
-  "API.HTTPHeaders.Access-Control-Allow-Methods" => ["PUT", "GET", "POST"],
-  "API.HTTPHeaders.Access-Control-Allow-Origin" => ["*"]
+  'API.HTTPHeaders.Access-Control-Allow-Credentials' => ['true'],
+  'API.HTTPHeaders.Access-Control-Allow-Methods' => ['PUT', 'GET', 'POST'],
+  'API.HTTPHeaders.Access-Control-Allow-Origin' => ['*'],
 }
